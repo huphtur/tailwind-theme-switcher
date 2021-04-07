d0493c66515ff53db34b9d5fffc8e2650d7513e2
M. Appelman <huphtur@icloud.com>
2021-04-06 17:03:17 -0700

    tweaks for Tailwind 2.1 JIT ⏳

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  mode: 'jit',
   purge: {
-    mode: 'all',
-    content: ['./public/**/*.html'],
+    content: ['./public/**/*.html','./safelist.txt'],
   },
   theme: {
     colors: {},
